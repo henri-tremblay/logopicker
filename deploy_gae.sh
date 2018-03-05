d4d8d88440d11a779b1ade34746bfc40588df3a8
Anthony Dahanne <anthony.dahanne@gmail.com>
2018-03-05 17:01:17 -0500

    GAE : * add gae deploy script

---
+++ deploy_gae.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# to exit in case of error
+set -e
+# to see what's going on
+set -v
+
+version="0.0.1-SNAPSHOT"
+
+echo "Deploying to GAE"
+
+# Build the release if not there
+if [ ! -f "target/logopicker-${version}.war" ]; then
+    ./mvnw clean -Pprod appengine:deploy -DskipTests
+fi
+
+echo "Deployment done"
